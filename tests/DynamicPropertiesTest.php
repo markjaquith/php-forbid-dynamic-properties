0adf1e66530ac8e0326ce840f9b20ba3f1ea7ff9
Mark Jaquith <mark@jaquith.me>
2022-08-15 15:00:40 -0400

    DRY

--- tests/DynamicPropertiesTest.php
+++ tests/DynamicPropertiesTest.php
@@ -4,101 +4,28 @@ use App\Foo;
 use App\FooChild;
 use App\FooGrandchild;
 
-// Foo.
-test('Foo: Accessing dynamic properties is forbidden', function () {
-	$foo = new Foo;
+function getClassInstances() {
+	return [
+		'Foo' => new Foo,
+		'FooChild' => new FooChild,
+		'FooGrandchild' => new FooGrandchild,
+	];
+}
 
-	expect(function () use ($foo) {
-		$foo->dynamic;
-	})->toThrow('Accessing dynamic properties is forbidden');
-});
+test('Accessing dynamic properties is forbidden', function ($foo) {
+	expect(fn () => $foo->dynamic)->toThrow('Accessing dynamic properties is forbidden');
+})->with(fn () => getClassInstances());
 
-test('Foo: Setting dynamic properties is forbidden', function () {
-	$foo = new Foo;
+test('Setting dynamic properties is forbidden', function ($foo) {
+	expect(fn () => $foo->dynamic = 'dynamic')->toThrow('Setting dynamic properties is forbidden');
+})->with(fn () => getClassInstances());
 
-	expect(function () use ($foo) {
-		$foo->dynamic = 'dynamic';
-	})->toThrow('Setting dynamic properties is forbidden');
-});
+test('Checking dynamic properties is forbidden', function ($foo) {
+	expect(fn () => isset($foo->dynamic))->toThrow('Checking dynamic properties is forbidden');
+})->with(fn () => getClassInstances());
 
-test('Foo: Checking dynamic properties is forbidden', function () {
-	$foo = new Foo;
-
-	expect(function () use ($foo) {
-		isset($foo->dynamic);
-	})->toThrow('Checking dynamic properties is forbidden');
-});
-
-test('Foo: Unsetting dynamic properties is forbidden', function () {
-	$foo = new Foo;
-
-	expect(function () use ($foo) {
+test('Unsetting dynamic properties is forbidden', function ($foo) {
+	expect(function () use($foo) {
 		unset($foo->dynamic);
 	})->toThrow('Unsetting dynamic properties is forbidden');
-});
-
-// FooChild.
-test('FooChild: Accessing dynamic properties is forbidden', function () {
-	$foo = new FooChild;
-
-	expect(function () use ($foo) {
-		$foo->dynamic;
-	})->toThrow('Accessing dynamic properties is forbidden');
-});
-
-test('FooChild: Setting dynamic properties is forbidden', function () {
-	$foo = new FooChild;
-
-	expect(function () use ($foo) {
-		$foo->dynamic = 'dynamic';
-	})->toThrow('Setting dynamic properties is forbidden');
-});
-
-test('FooChild: Checking dynamic properties is forbidden', function () {
-	$foo = new FooChild;
-
-	expect(function () use ($foo) {
-		isset($foo->dynamic);
-	})->toThrow('Checking dynamic properties is forbidden');
-});
-
-test('FooChild: Unsetting dynamic properties is forbidden', function () {
-	$foo = new FooChild;
-
-	expect(function () use ($foo) {
-		unset($foo->dynamic);
-	})->toThrow('Unsetting dynamic properties is forbidden');
-});
-
-// FooGrandchild.
-test('FooGrandchild: Accessing dynamic properties is forbidden', function () {
-	$foo = new FooGrandchild;
-
-	expect(function () use ($foo) {
-		$foo->dynamic;
-	})->toThrow('Accessing dynamic properties is forbidden');
-});
-
-test('FooGrandchild: Setting dynamic properties is forbidden', function () {
-	$foo = new FooGrandchild;
-
-	expect(function () use ($foo) {
-		$foo->dynamic = 'dynamic';
-	})->toThrow('Setting dynamic properties is forbidden');
-});
-
-test('FooGrandchild: Checking dynamic properties is forbidden', function () {
-	$foo = new FooGrandchild;
-
-	expect(function () use ($foo) {
-		isset($foo->dynamic);
-	})->toThrow('Checking dynamic properties is forbidden');
-});
-
-test('FooGrandchild: Unsetting dynamic properties is forbidden', function () {
-	$foo = new FooGrandchild;
-
-	expect(function () use ($foo) {
-		unset($foo->dynamic);
-	})->toThrow('Unsetting dynamic properties is forbidden');
-});
+})->with(fn () => getClassInstances());
