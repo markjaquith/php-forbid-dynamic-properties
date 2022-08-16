9880eb13e5f6e0370dc1ccd9413ebd061fb86445
Mark Jaquith <mark@jaquith.me>
2022-08-16 09:24:32 -0400

    Mark up data better

--- tests/NormalAccessTest.php
+++ tests/NormalAccessTest.php
@@ -13,10 +13,15 @@ test('FooChild data is correct', function ($foo) {
 	expect($foo->getChildPrivate())->toBe('childPrivate');
 	expect($foo->getChildProtected())->toBe('childProtected');
 	expect($foo->getChildPublic())->toBe('childPublic');
-})->with([new FooChild, new FooGrandchild]);
+})->with([
+	'FooChild' => new FooChild,
+	'FooGrandchild' => new FooGrandchild,
+]);
 
 test('FooGrandchild data is correct', function ($foo) {
 	expect($foo->getGrandchildPrivate())->toBe('grandchildPrivate');
 	expect($foo->getGrandchildProtected())->toBe('grandchildProtected');
 	expect($foo->getGrandchildPublic())->toBe('grandchildPublic');
-})->with([new FooGrandchild]);
+})->with([
+	'FooGrandchild' => new FooGrandchild,
+]);
